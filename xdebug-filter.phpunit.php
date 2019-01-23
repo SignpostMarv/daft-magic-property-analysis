e3d687899574a70d15a645fa9835eeb7297e3afd
SignpostMarv <me@signpostmarv.name>
2019-01-23 18:33:42 +0000

    attempting to resolve scrutinizer issue

--- xdebug-filter.phpunit.php
+++ xdebug-filter.phpunit.php
@@ -1,7 +1,10 @@
 <?php
-
 declare(strict_types=1);
-if ( ! \function_exists('xdebug_set_filter')) {
+if (
+    ! \function_exists('xdebug_set_filter') ||
+    ! defined('XDEBUG_FILTER_CODE_COVERAGE') ||
+    ! defined('XDEBUG_PATH_WHITELIST')
+) {
     return;
 }
 
