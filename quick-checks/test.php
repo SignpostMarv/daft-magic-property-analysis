53e88148ad0205cb8ef26289e951bf10841e9222
SignpostMarv <me@signpostmarv.name>
2019-01-23 18:23:42 +0000

    cleaning up bootstrapper

--- quick-checks/test.php
+++ quick-checks/test.php
@@ -8,42 +8,6 @@ namespace SignpostMarv\DaftMagicPropertyAnalysis\Tests;
 
 use SignpostMarv\DaftMagicPropertyAnalysis\DefinitionAssistant;
 
-$getter = function (string $property) : ? string {
-    return
-        in_array(
-            $property,
-            DaftMagicPropertyAnalysis\Fixtures\ucwordsPrefixedTypeInterface::PROPERTIES,
-            true
-        )
-            ? ('Get' . ucwords($property))
-            : null;
-};
-
-$setter = function (string $property) : ? string {
-    return
-        in_array(
-            $property,
-            DaftMagicPropertyAnalysis\Fixtures\ucwordsPrefixedTypeInterface::PROPERTIES,
-            true
-        )
-            ? ('Set' . ucwords($property))
-            : null;
-};
-
-DefinitionAssistant::RegisterType(
-    DaftMagicPropertyAnalysis\Fixtures\ucwordsPrefixedTypeInterface::class,
-    $getter,
-    $setter,
-    ...DaftMagicPropertyAnalysis\Fixtures\ucwordsPrefixedTypeInterface::PROPERTIES
-);
-
-DefinitionAssistant::RegisterType(
-    DaftMagicPropertyAnalysis\Fixtures\ucwordsPrefixedImplementation::class,
-    $getter,
-    $setter,
-    ...DaftMagicPropertyAnalysis\Fixtures\ucwordsPrefixedTypeInterface::PROPERTIES
-);
-
 function bat() : DaftMagicPropertyAnalysis\Fixtures\ucwordsPrefixedTypeInterface {
     return new DaftMagicPropertyAnalysis\Fixtures\ucwordsPrefixedImplementation();
 }
