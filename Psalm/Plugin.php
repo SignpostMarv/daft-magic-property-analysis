14562db8d3d81c5528812a6b9f52268316be7a61
SignpostMarv <me@signpostmarv.name>
2019-01-28 19:50:03 +0000

    php-cs-fixer

--- Psalm/Plugin.php
+++ Psalm/Plugin.php
@@ -7,24 +7,16 @@ declare(strict_types=1);
 namespace SignpostMarv\DaftMagicPropertyAnalysis\Psalm;
 
 use PhpParser\Node\Stmt\ClassLike;
-use PhpParser\Node\Identifier;
 use Psalm\Codebase;
-use Psalm\FileManipulation;
 use Psalm\FileSource;
 use Psalm\Internal\Analyzer\ClassLikeAnalyzer;
 use Psalm\Plugin\Hook\AfterClassLikeVisitInterface;
-use Psalm\Plugin\Hook\AfterClassLikeStoragePopulated;
 use Psalm\Storage\ClassLikeStorage;
 use Psalm\Storage\PropertyStorage;
-use Psalm\Type\Union;
-use Psalm\Type\Atomic\Mixed;
-use ReflectionMethod;
 use SignpostMarv\DaftMagicPropertyAnalysis\DefinitionAssistant;
-use SignpostMarv\DaftMagicPropertyAnalysis\Tests\DaftMagicPropertyAnalysis\Fixtures\ucwordsPrefixedTypeInterface;
 
 class Plugin implements AfterClassLikeVisitInterface
 {
-
     public static function afterClassLikeVisit(
        ClassLike $stmt,
        ClassLikeStorage $storage,
